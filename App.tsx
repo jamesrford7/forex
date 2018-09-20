2a74231ebcbbf54292a29f1de9a24f08eb4a16e5
jamesrford7 <jamesrford7@gmail.com>
2018-09-20 12:40:02 +0100

    Set up all the things

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import CurrencySelectPage from'./src/components/pages/CurrencySelectPage';
+
+const App = () => (
+	<CurrencySelectPage />
+)
+
+export default App;
---
+++ src/components/pages/CurrencySelectPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CurrencySelectPage = () => (
+    <View>
+        <Text>My Testsss</Text>
+    </View>
+)
+
+export default CurrencySelectPage;
